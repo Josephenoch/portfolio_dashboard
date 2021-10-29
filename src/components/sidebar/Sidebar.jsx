71d9647237417cd13f85a73d2a4a40c743b59e6a
Josephenoch <60913747+Josephenoch@users.noreply.github.com>
2021-10-29 01:33:39 +0100

    Sidebar Added
    This commit includes changes made to the sidebar as well as the deleted topbar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./sidebar.css"
+import { Home, Search, Assessment } from "@material-ui/icons"
+import logo from "../../assets/Webfolio.jpg"
+import { IconButton, InputAdornment, TextField } from "@material-ui/core";
 
 const Sidebar=(() =>{
+   const [searchText, setSearchText] = useState("")
     return (
-        <div>
-           {<h1>Hello World</h1>} 
+        <div className = "sidebar">
+            <div className = "sidebarWrapper">
+                <div className = "sidebarMenu">
+                    <div className = "logo"><img src = {logo} alt = "logo"></img></div>
+
+                        <TextField 
+                        type="search"
+                        size="small"
+                        value={searchText}
+                        onChange={(e) => {setSearchText(e.target.value)}}
+                        variant="outlined"
+                        InputProps={{
+                                startAdornment: (
+                                    <InputAdornment>
+                                        <IconButton>
+                                            <Search/>
+                                        </IconButton>
+                                    </InputAdornment>
+                                )
+                            }}
+                        />
+
+                    <ul className = "sidebarList">
+                        <li className="sidebarListItem"><Home />Home</li>
+                        <li className="sidebarListItem"><Assessment />Dashboard</li>
+                        <li className="sidebarListItem"><Home />Home</li>
+                        <li className="sidebarListItem"><Home />Home</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 })
