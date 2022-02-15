7f00842a4656db1f36e1845e5ffdea63b9c176ad
Josephenoch <60913747+Josephenoch@users.noreply.github.com>
2022-02-15 15:18:36 +0100

    modified skills.js file

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import {React, useEffect, useState} from 'react';
 import PageHeader from '../PageHeader';
 
 const Skill = () => {
+  const [datas, setData] = useState([])
+  useEffect(() =>{
+    fetch("https://reqres.in/api/users")
+    .then(response => response.json() )
+    .then(data => setData(data.data))
+  },[]);
   return (
     <div>
         <PageHeader title="Skills"/>
+        {
+          datas.map((data)=>{
+          {console.log(data.email)}
+          })
+        }
     </div>
     );
 }
